d0cb13c122854032e060e5edf580008ee2878129
camilo andres ibarra yepes <cibarra@grupoalto.com>
2019-10-29 23:55:12 -0500

    Initial Commit

---
+++ DataBindingMVVMExample/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  DataBindingMVVMExample
+//
+//  Created by camilo andres ibarra yepes on 29/10/19.
+//  Copyright © 2019 camilo andres ibarra yepes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var tableViewItems: UITableView!
+    @IBOutlet weak var textFieldNewItem: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onAddItem(_ sender: Any) {
+    }
+}
+
